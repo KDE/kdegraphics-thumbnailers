7cef7432332e10fc8bc8c0eb1428d8063cc0f8c2
Carl Schwan <carl@carlschwan.eu>
2025-05-29 22:13:48 +0200

    Adapt to new API

--- mobipocket/mobithumbnail.cpp
+++ mobipocket/mobithumbnail.cpp
@@ -9,7 +9,6 @@
 
 #include "mobithumbnail.h"
 #include <qmobipocket/mobipocket.h>
-#include <qmobipocket/qfilestream.h>
 
 #include <QFile>
 
@@ -24,8 +23,11 @@ MobiThumbnail::MobiThumbnail(QObject *parent, const QVariantList &args)
 
 KIO::ThumbnailResult MobiThumbnail::create(const KIO::ThumbnailRequest &request)
 {
-    Mobipocket::QFileStream f(request.url().toLocalFile());
-    Mobipocket::Document doc(&f);
+    QFile file(request.url().toLocalFile());
+    if (file.open(QFile::ReadOnly)) {
+        return KIO::ThumbnailResult::fail();
+    }
+    Mobipocket::Document doc(&file);
     if (!doc.isValid()) {
         return KIO::ThumbnailResult::fail();
     }
